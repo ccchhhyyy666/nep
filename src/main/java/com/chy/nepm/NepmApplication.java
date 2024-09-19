bb4a5609f14fc01c046ba49baab58e0733018d1d
chy <730618736@qq.com>
2024-09-19 12:42:41 +0800

    创建springboot基本项目

---
+++ src/main/java/com/chy/nepm/NepmApplication.java
@@ -0,0 +1,13 @@
+package com.chy.nepm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NepmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NepmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chy/nepm/NepmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chy.nepm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NepmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
